80481c3d42157dbca21896bc72cbe22234fa64b9
Lucas Pardue <lucaspardue.24.7@gmail.com>
2020-12-23 00:19:26 +0000

    pwa mwua

---
+++ precache-manifest.3441674d43ebc46ecaac0edfb1f6a096.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6e6ad0c92a07d708bc",
+    "url": "css/app.c07eddc1.css"
+  },
+  {
+    "revision": "5e51a9ca13928d28e85f",
+    "url": "css/chunk-vendors.7981fadf.css"
+  },
+  {
+    "revision": "1c5a70b482b5447ce62d39bb8634b3e5",
+    "url": "index.html"
+  },
+  {
+    "revision": "fc6e6ad0c92a07d708bc",
+    "url": "js/app.30645f81.js"
+  },
+  {
+    "revision": "5e51a9ca13928d28e85f",
+    "url": "js/chunk-vendors.7585e7d3.js"
+  },
+  {
+    "revision": "21ba7934a2610dbb526552ba0c6f9041",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a14bd646e6b59fc2100558fd1f8823c6",
+    "url": "standalone_data/draft-00/example_github.qlog.js"
+  },
+  {
+    "revision": "884316556552fc5b319f2077310f638d",
+    "url": "standalone_data/draft-00/quictrace_example_github.qlog"
+  },
+  {
+    "revision": "3046cabd4385970b01f9de2232da6378",
+    "url": "standalone_data/draft-01/doublevantage_100ms.qlog"
+  },
+  {
+    "revision": "c36559edffc6e3867f999a5cc5dd8e36",
+    "url": "standalone_data/draft-01/new_cid.qlog"
+  },
+  {
+    "revision": "f5c6314bd6c8c681c8f60b7def74f832",
+    "url": "standalone_data/draft-01/new_cid.qlog.js"
+  },
+  {
+    "revision": "77ca64c8457e05fcd40a87abd8b6df0b",
+    "url": "standalone_data/draft-01/parallel_10_50KB_f5.qlog"
+  },
+  {
+    "revision": "c62998530a427a84b7399d05a112965b",
+    "url": "standalone_data/draft-01/spin_bit.qlog"
+  },
+  {
+    "revision": "a573a380830fb7fdfb2b1ae5532fa209",
+    "url": "standalone_data/draft-01/spin_bit.qlog.js"
+  },
+  {
+    "revision": "ff1fa53fdc41b0ea0730ca706e4e0dc9",
+    "url": "standalone_data/prespec/ngtcp2_multistreams_server_10ploss.qlog.js"
+  },
+  {
+    "revision": "20e0d334a7d64e38e705b61e5dd5b5dd",
+    "url": "standalone_data/prespec/ngtcp2_multistreams_server_losscomparison.qlog.js"
+  },
+  {
+    "revision": "227ab21e81209fe1a31f1ba0caaa04b5",
+    "url": "standalone_data/prespec/ngtcp2_multistreams_server_noloss.qlog.js"
+  },
+  {
+    "revision": "3634db2ef873e4fded4c960cae51414a",
+    "url": "standalone_data/prespec/ngtcp2_pcap1.qlog.js"
+  },
+  {
+    "revision": "2c1cf1b80803891be27c612eb3657a19",
+    "url": "standalone_data/prespec/quictracker_handshake_v6_quicker_20181219.qlog.js"
+  },
+  {
+    "revision": "baee3e7005a526e807b51af902dbadcf",
+    "url": "standalone_data/tcp/wikipedia_Playstation.json"
+  }
+]);
